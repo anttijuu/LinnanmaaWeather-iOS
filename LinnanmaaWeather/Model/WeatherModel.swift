a2094d6c5a08bfa304a81a9ff0a53d53d96cd09e
Antti Juustila <antti.juustila@gmail.com>
2023-02-19 20:00:49 +0200

    Refactored WeatherModel

--- LinnanmaaWeather/Model/WeatherModel.swift
+++ LinnanmaaWeather/Model/WeatherModel.swift
@@ -17,15 +17,9 @@ class WeatherModel: ObservableObject {
 	func getWeather() async {
 		do {
 			let url = URL(string: "http://weather.willab.fi/weather.json")!
-			if let data = try await doGet(from: url) {
-				let weather = try parse(from: data)
-				measurements.removeAll()
-				measurements.append(Measurement(name: NSLocalizedString("Temperature", comment: ""), value: weather.tempnow, unit: " °C"))
-				measurements.append(Measurement(name: NSLocalizedString("Humidity", comment: ""), value: Double(weather.humidity), unit: "%"))
-				measurements.append(Measurement(name: NSLocalizedString("Air pressure", comment: ""), value: weather.airpressure, unit: " hPa"))
-				measurements.append(Measurement(name: NSLocalizedString("Wind direction", comment: ""), value: Double(weather.winddir), unit: "°"))
-				measurements.append(Measurement(name: NSLocalizedString("Wind speed", comment: ""), value: weather.windspeed, unit: " m/s"))
-				timeStamp = weather.timestamp
+			if let data = try await getWeather(from: url) {
+				let weather = try parseWeather(from: data)
+				updateMeasurements(from: weather)
 				status = NSLocalizedString("Weather updated", comment: "Successfully updated the weather data")
 			}
 		} catch WeatherError.httpError(let code) {
@@ -37,7 +31,7 @@ class WeatherModel: ObservableObject {
 		}
 	}
 
-	private func doGet(from url: URL) async throws -> Data? {
+	private func getWeather(from url: URL) async throws -> Data? {
 		let (data, response) = try await URLSession.shared.data(from: url)
 		if let httpResponse = response as? HTTPURLResponse {
 			if httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 {
@@ -49,13 +43,23 @@ class WeatherModel: ObservableObject {
 		return nil
 	}
 
-	private func parse(from data: Data) throws -> Weather {
+	private func parseWeather(from data: Data) throws -> Weather {
 		do {
 			let weather = try JSONDecoder().decode(Weather.self, from: data)
 			return weather
 		} catch {
 			throw WeatherError.parseError(error.localizedDescription)
 		}
+	}
+
+	private func updateMeasurements(from weather: Weather) {
+		measurements.removeAll()
+		measurements.append(Measurement(name: NSLocalizedString("Temperature", comment: ""), value: weather.tempnow, unit: " °C"))
+		measurements.append(Measurement(name: NSLocalizedString("Humidity", comment: ""), value: Double(weather.humidity), unit: "%"))
+		measurements.append(Measurement(name: NSLocalizedString("Air pressure", comment: ""), value: weather.airpressure, unit: " hPa"))
+		measurements.append(Measurement(name: NSLocalizedString("Wind direction", comment: ""), value: Double(weather.winddir), unit: "°"))
+		measurements.append(Measurement(name: NSLocalizedString("Wind speed", comment: ""), value: weather.windspeed, unit: " m/s"))
+		timeStamp = weather.timestamp
 	}
 
 }
